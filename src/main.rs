fad598a5f3a668c2a735f3c8292845c5ab1503a0
Ocky <ocky@algonet.xyz>
2026-04-12 14:47:15 +0200

    Added pipeline for fuel efficiency removed events.

--- src/main.rs
+++ src/main.rs
@@ -322,6 +322,13 @@ async fn main() -> Result<(), anyhow::Error> {
                         sequential.clone(),
                     )
                     .await?;
+
+                indexer
+                    .sequential_pipeline(
+                        world::FuelEfficiencyRemovedHandler::new(&context),
+                        sequential.clone(),
+                    )
+                    .await?;
             }
         }
     }
