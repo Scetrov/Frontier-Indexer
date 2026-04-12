59bfeb8d71eea7a30c16978cb84d9c4e683f6e63
Ocky <ocky@algonet.xyz>
2026-04-12 22:17:09 +0200

    Implemented handler for fuel deleted events.

--- src/handlers/world.rs
+++ src/handlers/world.rs
@@ -51,6 +51,7 @@ pub use primitives::fuel::fuel_burning_started_handler::*;
 pub use primitives::fuel::fuel_burning_stopped_handler::*;
 pub use primitives::fuel::fuel_burning_updated_handler::*;
 pub use primitives::fuel::fuel_config_handler::*;
+pub use primitives::fuel::fuel_deleted_handler::*;
 pub use primitives::fuel::fuel_efficiency_removed_handler::*;
 pub use primitives::fuel::fuel_efficiency_set_handler::*;
 
--- src/handlers/world/primitives/fuel.rs
+++ src/handlers/world/primitives/fuel.rs
@@ -2,5 +2,6 @@ pub mod fuel_burning_started_handler;
 pub mod fuel_burning_stopped_handler;
 pub mod fuel_burning_updated_handler;
 pub mod fuel_config_handler;
+pub mod fuel_deleted_handler;
 pub mod fuel_efficiency_removed_handler;
 pub mod fuel_efficiency_set_handler;
