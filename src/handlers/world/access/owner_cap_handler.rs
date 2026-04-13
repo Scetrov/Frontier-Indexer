b486a2c0af59e29022f038f609201888c71ac58e
Ocky <ocky@algonet.xyz>
2026-04-13 22:35:25 +0200

    Refactored owner caps to use updated context.

--- src/handlers/world/access/owner_cap_handler.rs
+++ src/handlers/world/access/owner_cap_handler.rs
@@ -1,8 +1,6 @@
 use async_trait::async_trait;
-use move_core_types::account_address::AccountAddress;
 use std::collections::hash_map::Entry;
-use std::collections::{HashMap, HashSet};
-use std::str::FromStr;
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use diesel::prelude::*;
@@ -20,53 +18,23 @@ use sui_indexer_alt_framework::pipeline::Processor;
 use sui_indexer_alt_framework::postgres::{Connection, Db};
 use sui_indexer_alt_framework::types::full_checkpoint_content::Checkpoint;
 
-use crate::handlers::is_indexed_tx;
 use crate::models::world::StoredOwnerCap;
 
 use crate::AppContext;
 
 pub struct OwnerCapHandler {
     ctx: AppContext,
-    package_set: HashSet<AccountAddress>,
 }
 
 impl OwnerCapHandler {
     pub fn new(ctx: &AppContext) -> Self {
-        let package_set: HashSet<AccountAddress> = ctx
-            .get_world_package_strings()
-            .iter()
-            .filter_map(|s| AccountAddress::from_str(s).ok())
-            .collect();
-
-        Self {
-            ctx: ctx.clone(),
-            package_set,
-        }
+        Self { ctx: ctx.clone() }
     }
 
     fn is_owner_cap(&self, obj: &Object) -> bool {
         let module_name = "access";
         let struct_name = "OwnerCap";
-
-        if let Some(move_type) = obj.type_() {
-            if let Some(tag) = move_type.other() {
-                if !self.package_set.contains(&tag.address) {
-                    return false;
-                }
-
-                if tag.module.as_str() != module_name {
-                    return false;
-                }
-
-                if tag.name.as_str() != struct_name {
-                    return false;
-                }
-
-                return true;
-            }
-        }
-
-        false
+        self.ctx.is_world_object(obj, module_name, struct_name)
     }
 }
 
@@ -86,7 +54,7 @@ impl Processor for OwnerCapHandler {
         let cp_sequence = checkpoint.summary.sequence_number as i64;
 
         for tx in &checkpoint.transactions {
-            if !is_indexed_tx(tx, &checkpoint.object_set, &self.ctx) {
+            if !self.ctx.is_indexed_tx(tx, &checkpoint.object_set) {
                 continue;
             }
 
