b8744f05383abcf8993e9fd0d7092243e898e647
Ocky <ocky@algonet.xyz>
2026-04-14 23:49:33 +0200

    Added migration for item minted events.

---
+++ migrations/2026-04-14-211020-0000_events_item_minted/up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS indexer.events_item_minted (
+  event_id          VARCHAR(66)   NOT NULL,
+  occurred_at       TIMESTAMPTZ   NOT NULL,
+  item_id           VARCHAR(20)   NOT NULL,
+  type_id           BIGINT        NOT NULL,
+  quantity          BIGINT        NOT NULL,
+  assembly_id       VARCHAR(66)   NOT NULL,
+  assembly_item_id  VARCHAR(20)   NOT NULL,
+  character_id      VARCHAR(66)   NOT NULL,
+  character_item_id VARCHAR(20)   NOT NULL,
+  PRIMARY KEY (event_id, occurred_at)
+);
+
+SELECT public.create_hypertable('indexer.events_item_minted', 'occurred_at');
