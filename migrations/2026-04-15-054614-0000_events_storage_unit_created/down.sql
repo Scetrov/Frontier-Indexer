7dbe516cad19d192d70810f29158fae611e894ac
Ocky <ocky@algonet.xyz>
2026-04-15 08:20:28 +0200

    Added migration for storage unit created events.

---
+++ migrations/2026-04-15-054614-0000_events_storage_unit_created/down.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS indexer.events_storage_unit_created;
---
+++ migrations/2026-04-15-054614-0000_events_storage_unit_created/up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS indexer.events_storage_unit_created (
+    event_id      VARCHAR(100)  NOT NULL,
+    occurred_at   TIMESTAMPTZ   NOT NULL,
+    id            VARCHAR(66)   NOT NULL,
+    item_id       VARCHAR(20)   NOT NULL,
+    tenant        TEXT          NOT NULL,
+    type_id       BIGINT        NOT NULL,
+    owner_cap_id  VARCHAR(66)   NOT NULL,
+    PRIMARY KEY (event_id, occurred_at)
+);
+
+SELECT public.create_hypertable('indexer.events_storage_unit_created', 'occurred_at');
