52a9f2b75773ae5ba38c89b9b1cc8260ae1bca80
Ocky <ocky@algonet.xyz>
2026-04-15 23:15:28 +0200

    Implemented models for inventory entries.

---
+++ src/models/world/primitives/inventories/inventory_entries.rs
@@ -0,0 +1,44 @@
+use serde::Deserialize;
+
+use diesel::prelude::*;
+
+use crate::schema::indexer::inventory_entries;
+use sui_indexer_alt_framework::FieldCount;
+
+#[derive(Deserialize)]
+pub struct MoveItemEntry {
+    pub tenant: String,
+    pub type_id: u64,
+    pub item_id: u64,
+    pub volume: u64,
+    pub quantity: u32,
+}
+
+#[derive(Insertable, Debug, Clone, FieldCount)]
+#[diesel(table_name = inventory_entries)]
+pub struct StoredInventoryEntry {
+    pub inventory_id: String,
+    pub type_id: i64,
+    pub item_id: i64,
+    pub volume: i64,
+    pub quantity: i64,
+    pub checkpoint_updated: i64,
+}
+
+impl StoredInventoryEntry {
+    pub fn from_object(
+        &self,
+        entry: &MoveItemEntry,
+        inventory_id: String,
+        checkpoint_updated: i64,
+    ) -> Self {
+        Self {
+            inventory_id,
+            type_id: entry.type_id as i64,
+            item_id: entry.item_id as i64,
+            volume: entry.volume as i64,
+            quantity: entry.quantity as i64,
+            checkpoint_updated,
+        }
+    }
+}
